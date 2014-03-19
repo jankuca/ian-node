0f7833ab4fc6d5b3c23bf28b08b720a3da60762a
jankuca <jan@jankuca.com>
2014-03-19 19:43:03 +0100

    initial commit

---
+++ test/_globals.js
@@ -0,0 +1,35 @@
+
+global.expect = require('expect.js');
+
+
+var timeout_queue = [];
+
+global.setTimeout = function (callback, timeout) {
+  return timeout_queue.push({
+    timeout: timeout,
+    callback: callback
+  });
+};
+
+global.clearTimeout = function (id) {
+  if (!id) return;
+  timeout_queue[id - 1] = null;
+};
+
+global.setTimeout.clear = function () {
+  timeout_queue.forEach(function (item, index) {
+    timeout_queue[index] = null;
+  });
+};
+
+global.setTimeout.flush = function (ms) {
+  timeout_queue.forEach(function (item, index) {
+    if (!item) return;
+    if (typeof ms !== 'number' || item.timeout <= ms) {
+      timeout_queue[index] = null;
+      item.callback.call(null);
+    } else if (typeof ms === 'number') {
+      item.timeout -= ms;
+    }
+  });
+};
